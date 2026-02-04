71138cd1b62cb19e3efe16dbcf771fe98e86e729
nas7062 <nas7062@naver.com>
2026-02-04 16:19:56 +0900

    chore : components 폴더 위치 변경

---
+++ app/viewer/page.tsx
@@ -0,0 +1,12 @@
+import ThreeViewer from "../../components/ThreeViewer";
+
+export default function ViewerPage() {
+  return (
+    <div className="flex w-screen h-screen">
+      <div className="flex-1">
+        <ThreeViewer />
+      </div>
+
+    </div>
+  )
+}
