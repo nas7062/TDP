2872bae105e4982d2bf1f99643a4f77e337d3dfc
nas7062 <nas7062@naver.com>
2026-02-04 13:20:15 +0900

    feat : header 생성

---
+++ app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Header() {
+  const nickname = '홍길동님'
+  return (
+    <header className="h-16 relative flex items-center px-4 justify-between  bg-linear-to-r from-blue-50 via-blue-100 to-blue-50 ">
+      <Link href="/">
+        <h1 className="text-4xl  font-semibold cursor-pointer">SIMVEX</h1>
+      </Link>
+      <div className="flex gap-2 justify-center items-center">
+        <p className="font-semibold">{nickname}</p>
+        <div className="h-10 w-10 rounded-full bg-gray-200">
+        </div>
+      </div>
+    </header>
+  )
+}
